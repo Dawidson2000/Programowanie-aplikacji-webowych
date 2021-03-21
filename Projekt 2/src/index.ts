83bb69e26cb27f68517bb62a8d26e5aef874466e
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-03-21 14:20:08 +0100

    Dodanie zagrania dźwięku

---
+++ Projekt 2/src/index.ts
@@ -0,0 +1,19 @@
+let clapSound: HTMLAudioElement;
+
+appStart();
+
+function appStart(): void{
+    document.body.addEventListener('keypress', onKeyDown);
+}
+
+function onKeyDown(event: KeyboardEvent): void{
+    const key = event.key;
+    const time = event.timeStamp;
+
+    clapSound.play();
+}
+
+function getAudioTags()
+{
+    clapSound = document.querySelector('[data-sound="clap"]');
+}
