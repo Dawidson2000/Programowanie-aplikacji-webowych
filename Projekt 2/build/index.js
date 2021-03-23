6147e4cb9bd324d9249017c5c4fd094b18b11c48
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-03-23 11:28:04 +0100

    Dodanie funkcjonalności grania na klawiaturze

--- Projekt 2/build/index.js
+++ Projekt 2/build/index.js
@@ -1,14 +1,69 @@
-var clapSound;
+var channel1 = [];
+var activeChannel = false;
+var audioTags = [];
 appStart();
 function appStart() {
-    document.body.addEventListener('keypress', onKeyDown);
-}
-function onKeyDown(event) {
-    var key = event.key;
-    var time = event.timeStamp;
+    document.addEventListener('keypress', onKeyPress);
+    var startChannel1 = document.querySelector('#startRecord1');
+    var stopChannel1 = document.querySelector('#stopRecord1');
+    var playChannel1 = document.querySelector('#playRecord1');
+    startChannel1.addEventListener('click', startRecord);
+    stopChannel1.addEventListener('click', stopRecord);
+    playChannel1.addEventListener('click', playRecord);
     getAudioTags();
-    clapSound.play();
+    renderButtons();
 }
 function getAudioTags() {
-    clapSound = document.querySelector('[data-sound="clap"]');
+    var audio = document.querySelectorAll('audio');
+    audio.forEach(function (element) {
+        audioTags.push(element);
+    });
+}
+function renderButtons() {
+    var buttonsDiv = document.getElementById('buttons');
+    audioTags.forEach(function (element) {
+        var button = document.createElement('button');
+        button.className = 'soundButton';
+        button.innerText = element.dataset.key;
+        button.id = element.dataset.key;
+        buttonsDiv.appendChild(button);
+    });
+}
+function startRecord() {
+    activeChannel = true;
+    channel1 = [];
+}
+function stopRecord() {
+    activeChannel = false;
+}
+function playRecord() {
+    channel1.forEach(function (sound) {
+        setTimeout(function () { return playSound(sound.key); }, sound.time);
+    });
+}
+function onKeyPress(ev) {
+    console.log(ev);
+    var key = ev.key;
+    var time = Date.now();
+    console.log(time + "XD");
+    if (activeChannel)
+        channel1.push({ key: key, time: time });
+    playSound(key);
+    //console.log(channel1);
+}
+function playSound(key) {
+    try {
+        var sound = document.querySelector("[data-key=" + "'" + key + "']");
+        sound.currentTime = 0;
+        sound.play();
+        playAnimation(key);
+    }
+    catch (Error) {
+    }
+}
+function playAnimation(key) {
+    document.getElementById(key).classList.toggle('onClick');
+    setTimeout(function () {
+        document.getElementById(key).classList.toggle('onClick');
+    }, 150);
 }
--- Projekt 2/src/index.ts
+++ Projekt 2/src/index.ts
@@ -1,19 +1,88 @@
-let clapSound: HTMLAudioElement;
+let channel1: any[] = [];
+let activeChannel: boolean = false;
 
+const audioTags: HTMLAudioElement[] = [];
 appStart();
 
-function appStart(): void{
-    document.body.addEventListener('keypress', onKeyDown);
+function appStart() {
+    document.addEventListener('keypress', onKeyPress);
+    
+    const startChannel1 = document.querySelector('#startRecord1');
+    const stopChannel1 = document.querySelector('#stopRecord1');
+    const playChannel1 = document.querySelector('#playRecord1');
+    
+    startChannel1.addEventListener('click', startRecord);
+    stopChannel1.addEventListener('click', stopRecord);
+    playChannel1.addEventListener('click', playRecord);
+
+
+    getAudioTags();
+    renderButtons();
 }
 
-function onKeyDown(event: KeyboardEvent): void{
-    const key = event.key;
-    const time = event.timeStamp;
-    getAudioTags()
-    clapSound.play();
+function getAudioTags(): void{
+    const audio = document.querySelectorAll('audio');
+    audio.forEach((element) => {
+        audioTags.push(element);
+    });  
 }
 
-function getAudioTags()
-{
-    clapSound = document.querySelector('[data-sound="clap"]');
+function renderButtons(): void{
+    const buttonsDiv = document.getElementById('buttons');
+
+    audioTags.forEach((element) => {
+        let button = document.createElement('button');
+        button.className = 'soundButton';
+        button.innerText = element.dataset.key;
+        button.id = element.dataset.key;
+        buttonsDiv.appendChild(button);
+    })
+}
+
+function startRecord(): void {
+    activeChannel = true;
+    channel1 = [];
+}
+
+function stopRecord(): void {
+    activeChannel = false;
+}
+
+function playRecord(): void {
+    channel1.forEach(sound => {
+        setTimeout(() => playSound(sound.key), sound.time)
+    })
+
+}
+
+function onKeyPress(ev: KeyboardEvent): void {
+    console.log(ev);
+    const key = ev.key;
+    const time = Date.now();
+    console.log(time + "XD");
+
+    if(activeChannel)
+        channel1.push({ key, time });
+
+    playSound(key);
+    //console.log(channel1);
+}
+
+function playSound(key: string): void {
+    try{
+        const sound: HTMLAudioElement = document.querySelector("[data-key=" + "'" + key + "']");
+        sound.currentTime = 0;
+        sound.play();
+        playAnimation(key);
+    }
+        catch(Error){
+    }
+}
+
+function playAnimation(key: string): void{
+    document.getElementById(key).classList.toggle('onClick');
+
+    setTimeout( () => {
+        document.getElementById(key).classList.toggle('onClick');
+    }, 150);
 }
