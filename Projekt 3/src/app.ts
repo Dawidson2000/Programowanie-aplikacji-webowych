d17da1b03c2cf232ef607b1e8cf94dbef1cdb6ca
Dawidson2000 <37578786+Dawidson2000@users.noreply.github.com>
2021-04-14 19:27:46 +0200

    Ostylowanie

--- Projekt 3/src/app.ts
+++ Projekt 3/src/app.ts
@@ -30,33 +30,87 @@ export class App {
     }
 
     checkInputValue(city: string): boolean {
-        if(city && (this.cityTab.find(el => el === city) === undefined))
+        if(city && (this.cityTab.find(el => el === city.toLowerCase()) === undefined))
             return true;
         else
             return false;
     }
 
     async addCity(city: string) {
-        
-        if(this.checkInputValue(city)){           
-            const weather = await this.getWeather(city);
-            this.renderCityContainer(weather);
-            
-            this.cityTab.push(city);
-            console.log(this.cityTab);
+        const weather = await this.getWeather(city);
+        if(weather.name){
+            if(this.checkInputValue(city)){                      
+                this.renderCityContainer(weather);
+                
+                this.cityTab.push(city.toLowerCase());
+                console.log(this.cityTab);
 
-            this.saveData(this.cityTab);
+                this.saveData(this.cityTab);
+            }
         }
     }
 
     renderCityContainer(cityWeather: any) {
         const container = document.getElementById('cities');
+        console.log(cityWeather);
 
-        let cityContainer = document.createElement('div');
-            cityContainer.innerText = `${cityWeather.name}: ${cityWeather.main.temp}`;
-            container.appendChild(cityContainer);
+        const cityContainer = document.createElement('div');
+            cityContainer.className = 'card';
+        
+        const cityName = document.createElement('div');
+            cityName.innerText = `${cityWeather.name}`;
+            cityName.className = 'cityName';
+            cityContainer.appendChild(cityName);
+
+        const temp = document.createElement('div');
+            temp.innerText = `${(cityWeather.main.temp- 273.15).toFixed(1)}°C`;
+            temp.className = 'temp';
+            cityContainer.appendChild(temp);
+            
+        const description = document.createElement('div');
+            description.innerText = `${cityWeather.weather[0].main}`;
+            description.className = 'description';
+            cityContainer.appendChild(description);
+
+        const line = document.createElement('div');
+            line.className = 'line';
+            cityContainer.appendChild(line);
+
+        const mainWeather = document.createElement('div');
+            mainWeather.innerText = `Wilgotność: ${cityWeather.main.humidity}%`;
+            mainWeather.className = 'mainWeather';
+            cityContainer.appendChild(mainWeather);
+
+        const humidity = document.createElement('div');
+            humidity.innerText = `Ciśnienie: ${cityWeather.main.pressure}hPA`;
+            humidity.className = 'mainWeather';
+            cityContainer.appendChild(humidity);
+
+        const line2 = document.createElement('div');
+            line2.className = 'line';
+            cityContainer.appendChild(line2);
+  
+        const windIcon = document.createElement('div');
+            const iconWind = document.createElement('i');
+                iconWind.className = "icon-wind";
+                windIcon.appendChild(iconWind);
+            windIcon.className = 'wind';
+            cityContainer.appendChild(windIcon);
+        
+        const windSpeed = document.createElement('div');
+            windSpeed.innerText = `Prędkość: ${cityWeather.wind.speed}m/s`;
+            windSpeed.className = 'windDescription';
+            cityContainer.appendChild(windSpeed);
+            
+        const windDirection = document.createElement('div');
+            windDirection.innerText = `Kierunek: ${cityWeather.wind.deg}°`;
+            windDirection.className = 'windDescription2';
+            cityContainer.appendChild(windDirection);
+
+        container.prepend(cityContainer);
+              
     }
-         
+        
     saveData(data: any) {
         localStorage.setItem('cityTab', JSON.stringify(data));
     }
@@ -64,10 +118,12 @@ export class App {
     getData() {
         const data = JSON.parse(localStorage.getItem('cityTab'));
     
-        if(data.length > 0){
-            data.forEach((city: string) => {
-                this.addCity(city);
-            })
+        if(data){
+            if(data.length > 0 && data){
+                data.forEach((city: string) => {
+                    this.addCity(city);
+                })
+            }
         }
     }
 }
